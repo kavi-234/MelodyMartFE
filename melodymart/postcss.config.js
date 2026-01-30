a747d006bf885d13b598abaa3e86fd336064639f
kavi-234 <kprabuddhi2001@gmail.com>
2026-01-31 02:05:06 +0530

    updated ui

--- melodymart/postcss.config.js
+++ melodymart/postcss.config.js
@@ -1,11 +1,6 @@
 export default {
   plugins: {
-<<<<<<< Updated upstream
     tailwindcss: {},
     autoprefixer: {},
-=======
-    '@tailwindcss/postcss': {},
-    'autoprefixer': {},
->>>>>>> Stashed changes
   },
 }
